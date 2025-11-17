d765cdeb2f9a4078677e1635e76a5153c5885e36
tessadgroot <>
2025-11-17 19:55:32 +0000

    Standard Libraries

---
+++ mastering_python_modules_and_imports/standard_libraries/main.py
@@ -0,0 +1,25 @@
+import math
+
+# Calculating the natural logarithm of 10
+log_value = math.log(10)
+print("The natural logarithm of 10 is:", log_value)
+
+# Calculating the factorial of 5
+factorial_value = math.factorial(5)
+print("The factorial of 5 is:", factorial_value)
+
+# Finding the sine, cosine, and tangent of 30 degrees
+# First convert 30 degrees to radians
+radians = math.radians(30)
+sin_value = math.sin(radians)
+cos_value = math.cos(radians)
+tan_value = math.tan(radians)
+print("The sine of 30 degrees is:", sin_value)
+print("The cosine of 30 degrees is:", cos_value)
+print("The tangent of 30 degrees is:", tan_value)
+
+# Using ceil and floor on 9.2
+ceil_value = math.ceil(9.2)
+floor_value = math.floor(9.2)
+print("The ceiling value of 9.2 is:", ceil_value)
+print("The floor value of 9.2 is:", floor_value)
